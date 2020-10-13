a14723a64188f855351300e8300edb09cfd544c4
Nav Loveday <twelvetreeorchard@gmail.com>
2020-10-13 10:38:44 -0400

    Initial deploy heroku

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DATABASE_URL,  { 
-//     dialect: 'postgres',
-// });
-const sequelize = new Sequelize('campawesomesauce', 'postgres', 159864, {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL,  { 
+    dialect: 'postgres',
 });
+// const sequelize = new Sequelize('campawesomesauce', 'postgres', 159864, {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
 sequelize.authenticate()
     .then(
         function () {
